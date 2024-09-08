a53df82f90030e58dda0dffa0f5a39aa589d0043
QIAF <qonitaafifah3@gmail.com>
2024-09-09 06:36:28 +0700

    fixed for testing

--- src/components/BiodataForm/StudentFileData.jsx
+++ src/components/BiodataForm/StudentFileData.jsx
@@ -3,16 +3,6 @@ import { Input } from "../Ui/Form/Input";
 import { ErrMsg } from "../Error/ErrMsg";
 
 function StudentFileData({ error, setFormData, formData }) {
-  // function handleFilePdf(e) {
-  // 	// oper data ke form data yang ada pada Form.jsx
-  // 	const { name, value, files } = e.target;
-  // 	setFormData((prevFormData) => ({
-  // 		...prevFormData,
-  // 		[name]: value,
-  // 		studentDocument: files ? files[0] : prevFormData.studentDocument, // Menambahkan file jika ada
-  // 	}));
-  // }
-
   function handleFilePdf(e) {
     const { name, files } = e.target;
     setFormData((prevFormData) => ({
@@ -34,8 +24,7 @@ function StudentFileData({ error, setFormData, formData }) {
               <div className="mb-3">
                 <label htmlFor="student_picture" className="form-label">
                   <b>
-                    Siswa diwajibkan untuk mengunggah dokumen PDF dengan format
-                    nama file menggunakan Nama Lengkap Anda:
+                    Masukkan foto 3x4 calon siswa
                     {!formData.student_picture && (
                       <span className="required">*</span>
                     )}
